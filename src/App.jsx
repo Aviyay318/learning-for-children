e7b1aa55b9c5f7498a93228aa82d9ef99c086c32
Aviya Yechezkel <aviyay@edu.acc.ac.il>
2025-01-23 15:20:36 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 
 import './App.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import SignUp from "./pages/SignUp.jsx";
+import SignUp from "./pages/signUp/SignUp.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import Homepage from "./pages/Homepage.jsx";
-import Content from "./pages/Content.jsx";
-import Profile from "./pages/Profile.jsx";
+import Homepage from "./pages/homePage/Homepage.jsx";
+import Content from "./pages/content/Content.jsx";
+import Profile from "./pages/profile/Profile.jsx";
 
 function App() {
 
