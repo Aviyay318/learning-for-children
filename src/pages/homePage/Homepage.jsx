88019a149e2fc3413ffc936ca41b1dedd87e5570
ramrevivo <ramrevivo0@gmail.com>
2025-04-20 21:38:18 +0300

    ADDED NEW COOL SHIT 4

--- src/pages/homePage/Homepage.jsx
+++ src/pages/homePage/Homepage.jsx
@@ -9,7 +9,6 @@ import AmericanQuestion from "../../components/Math/AmericanQuestion/AmericanQue
 import CompleteTheBoard from "../../components/Math/CompleteTheBoard/CompleteTheBoard.jsx";
 import Tutorial from "../../components/Tutorial/Tutorial.jsx";
 import {useTutorial} from "../../hooks/uiHooks/useTutorial.js";
-import EmailComponent from "../../components/EmailComponent.jsx";
 
 export default function Homepage() {
     const location = useLocation();
@@ -45,7 +44,6 @@ export default function Homepage() {
                 {/*        <h3>טוען רמה...</h3>*/}
                 {/*    )}*/}
                 {/*</div>*/}
-                <EmailComponent/>
             </div>
         </div>
     );
