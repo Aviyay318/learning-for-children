19930a175f6e53860864e26e2c2747a7a5fedb61
Aviya Yechezkel <aviyay@edu.acc.ac.il>
2025-05-07 14:14:40 +0300

    add by raviol

--- src/components/Guide/Guide.jsx
+++ src/components/Guide/Guide.jsx
@@ -1,25 +1,25 @@
 import useApi from "../../hooks/apiHooks/useApi.js";
-import {GET_GUIDE} from "../../utils/Constants.js";
-import {useEffect} from "react";
+import { GET_GUIDE } from "../../utils/Constants.js";
+import { useEffect } from "react";
 
-export default function Guide(){
-
+export default function Guide() {
     const { data, sendRequest } = useApi(GET_GUIDE, "GET", { minDelay: 0 });
 
-    const getGuide = async () => {
-        await sendRequest();
-    };
-
     useEffect(() => {
-        getGuide()
-        console.log(data)
-
+        sendRequest();
     }, []);
 
-
     return (
-        <label>
-            {data && data}
-        </label>
-    )
+        <div style={{ direction: "rtl", padding: "1rem", background: "#fff8dc", borderRadius: "12px" }}>
+            {data ? (
+                <div
+                    dangerouslySetInnerHTML={{ __html: data }}
+                    style={{ fontSize: "1.1rem", }}
+                />
+            ) : (
+                <p>טוען הוראות...</p>
+            )}
+
+        </div>
+    );
 }
